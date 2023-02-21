1804bdde67fa2305a6b4c2145ad79f2983e3bce6
lucaf1990 <117093377+lucaf1990@users.noreply.github.com>
2023-02-21 09:21:35 +0100

    k

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,19 +1,22 @@
-import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Star, StarFill } from "react-bootstrap-icons";
-import { useSelector, useDispatch } from "react-redux";
-import { ADD_TO_FAVOURITE, REMOVE_FROM_FAVOURITE } from "../redux/Action";
+import { Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Star, StarFill } from 'react-bootstrap-icons'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  addToFavouriteAction,
+  removeFromFavouriteAction,
+} from '../redux/actions'
 
 const Job = ({ data }) => {
-  const favourites = useSelector((state) => state.list);
-  const dispatch = useDispatch();
+  const favourites = useSelector((state) => state.favourite.list)
+  const dispatch = useDispatch()
 
-  const isFav = favourites.includes(data.company_name);
+  const isFav = favourites.includes(data.company_name)
 
   return (
     <Row
       className="mx-0 mt-3 p-3"
-      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+      style={{ border: '1px solid #00000033', borderRadius: 4 }}
     >
       <Col xs={3}>
         {isFav ? (
@@ -22,10 +25,7 @@ const Job = ({ data }) => {
             size={16}
             className="mr-2 my-auto"
             onClick={() =>
-              dispatch({
-                type: REMOVE_FROM_FAVOURITE,
-                payload: data.company_name,
-              })
+              dispatch(removeFromFavouriteAction(data.company_name))
             }
           />
         ) : (
@@ -33,12 +33,7 @@ const Job = ({ data }) => {
             color="gold"
             size={16}
             className="mr-2 my-auto"
-            onClick={() =>
-              dispatch({
-                type: ADD_TO_FAVOURITE,
-                payload: data.company_name,
-              })
-            }
+            onClick={() => dispatch(addToFavouriteAction(data.company_name))}
           />
         )}
         <Link to={`/${data.company_name}`}>{data.company_name}</Link>
@@ -49,7 +44,7 @@ const Job = ({ data }) => {
         </a>
       </Col>
     </Row>
-  );
-};
+  )
+}
 
-export default Job;
+export default Job
